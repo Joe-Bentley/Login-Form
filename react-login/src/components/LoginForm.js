9e2b02db7cdb39f352f024c3127426fca74961c8
Joe Bentley <joe.bentley.20@gmail.com>
2021-03-08 13:41:34 +0000

    Login Setup

---
+++ react-login/src/components/LoginForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function LoginForm({ Login, error }) {
+
+    const [details, setDetails] = useState({name:"", email: "", password: ""});
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        Login(details);
+    }
+
+    return(
+        <form onSubmit={submitHandler}>
+            <div className="form-inner">
+                <h2>Login</h2>
+                {/*Error! */}
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" id="name" onChange={event => setDetails({...details, name: event.target.value})} value={details.name}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">email:</label>
+                    <input type="email" name="email" id="email"onChange={event => setDetails({...details, email: event.target.value})} value={details.email}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">password:</label>
+                    <input type="password" name="password" id="password" onChange={event => setDetails({...details, password: event.target.value})} value={details.password}/>                          
+                </div>
+                <input type="submit" value="Login"/>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm;
